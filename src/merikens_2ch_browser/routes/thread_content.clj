7eafbed58be9a1efec54e7d05a91f1b04932675f
meriken2ch <meriken.2ch@gmail.com>
2016-01-10 17:52:01 -0800

    Fixed HTML parser

--- src/merikens_2ch_browser/routes/thread_content.clj
+++ src/merikens_2ch_browser/routes/thread_content.clj
@@ -716,6 +716,7 @@ The most recent version will (hopefully) be stored in the database."
           title (second (re-find #"<h1 [^>]+>(.*)</h1>" html-body))
           title (if title (unescape-html-entities title) title)
           thread-body (second (re-find #"(?s)<dl class=\"thread\"[^>]*>\n<dt>(.*)<br><br>\n</dl>" html-body))
+          thread-body (clojure.string/replace thread-body #"<div id=\"js--banners--thread\"[^>]*></div>" "")
           posts (clojure.string/split thread-body #"<br><br>\n<dt>")
           res-count (count posts)
           ; _ (log :debug "res-count:" res-count)
